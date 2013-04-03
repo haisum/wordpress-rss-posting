e231d6154aca9caf836d3b4c5a371b72f5ca17ef
hbot <codefreax@gmail.com>
2013-04-03 19:13:22 +0500

    fixing stupid indentation in feedcontroller

--- protected/controllers/FeedController.php
+++ protected/controllers/FeedController.php
@@ -99,8 +99,8 @@ class FeedController extends Controller
 				$postCat = new TermRelationships;
 				$postCat->term_taxonomy_id = $id;
 				$postCat->object_id = $post->ID;
-								$taxonomy[0]->count = $taxonomy[0]->count+1;
-								$taxonomy[0]->save(false);
+				$taxonomy[0]->count = $taxonomy[0]->count+1;
+				$taxonomy[0]->save(false);
 				$postCat->save(false);
 			}
 			$tags = explode("," ,$data["tags"]);
@@ -110,8 +110,8 @@ class FeedController extends Controller
 				if(count($term) > 0){
 					$term = $term[0];
 					$taxonomy = TermTaxonomy::model()->findAll("term_id=" . $term->term_id . " and taxonomy='post_tag'");
-										$taxonomy[0]->count = $taxonomy[0]->count+1;
-										$taxonomy[0]->save(false);
+					$taxonomy[0]->count = $taxonomy[0]->count+1;
+					$taxonomy[0]->save(false);
 					$id = $taxonomy[0]->term_taxonomy_id;
 				}
 				else{
@@ -126,8 +126,8 @@ class FeedController extends Controller
 					$taxonomy = new TermTaxonomy;
 					$taxonomy->term_id = $term->term_id;
 					$taxonomy->taxonomy = "post_tag";
-										$taxonomy->count = 1;
-										$taxonomy->save(false);
+					$taxonomy->count = 1;
+					$taxonomy->save(false);
 					$id = $taxonomy->term_taxonomy_id;
 				}
 				$postTag = new TermRelationships;
